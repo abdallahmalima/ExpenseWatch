8131047176d23d66910ec6ddb458b7aa61a0775e
abdallah <abdallahantony55.aa@gmail.com>
2023-04-08 01:25:00 +0300

    try to exclude images to ge removed on upload

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,18 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile << proc do |path|
+    if path =~ /\.(css|js)\z/
+      full_path = Rails.application.assets.resolve(path).to_path
+      if full_path.starts_with?(Rails.root.join('public/uploads/icons').to_path)
+        puts "Excluding asset: #{full_path}"
+        false
+      else
+        true
+      end
+    else
+      true
+    end
+  end
+  
